51216327263751f61e9c2a23e839c83fbe14b9d8
Ismay <ips@uclv.edu.cu>
2015-03-17 18:34:34 -0400

    README, CodeBook and R script

---
+++ CodeBook.md
@@ -0,0 +1,53 @@
+# Code book of the course project
+
+## Sources
+
+The transformed data set can be obtained here: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+The original explanation of the data set can be found here: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+## How to reproduce the tidy datasets
+
+The following operations was performed to create the 2 tidy datasets requested:
+
+- The UCI HAR Dataset is downloaded and unziped, if there are found neither the .zip file, nor the dataset folder.
+
+- Read from the "train" and "test" folder the measured variables (X_?.txt), the subjects ID (subjects_?.txt) and the activities (y_?.txt), and merge them respectively.
+
+- Export only the measured means and standard deviations to the file "training_and_test_set_merged.txt" with appropiate descriptive variable names read from the file "features.txt", located at the root level of the unzipped dataset. The variable names where clean-up too( all bracket where eliminated ).
+
+- The second tidy dataset was generated from the first one, computing the average of the means and standard deviations by subject and activity, and exporting the result to the file "average_of_mean_and_std.txt".
+
+## Tidy datasets explained
+
+The tables obtained are:
+
+1. training_and_test_set_merged.txt
+2. average_of_mean_and_std.txt
+
+All measured variables contain a "t" or "f" in front of the variables that means 't'ime domain signals, and 'f'recuency domain signals. 
+
+### training_and_test_set_merged.txt
+
+training_and_test_set_merged.txt contain a subset of the variables (mean and standard deviation) from the UCI HAR datasets (train and test) merged together. It contain 68 variables.
+
+1. Subject: literal ranging from 1 to 30
+2. Activity: one of the states measured
+    WALKING
+    WALKING_UPSTAIRS
+    WALKING_DOWNSTAIRS
+    SITTING
+    STANDING
+    LAYING
+3..68 Those are the means and standard deviations of the measured data. The variable names meaning are comprised of
+
+Body Accelaration (BodyAcc), 
+Gravity Acceleration (GravityAcc), 
+Body Angular Velocity (BodyGyro),
+
+the derived Jerk signals (Jerk), the 3-axial signal directions (-XYZ). And of ofcourse, the mean (-mean) and standard deviation (-std)
+
+### average_of_mean_and_std.txt
+
+average_of_mean_and_std.txt contain the average of the means and standard deviations of the measured variables. It has 
+the same 68 variables of the former tidy dataset, and 180 rows represented by the combinations of each subject (30) with each activity (6).
---
+++ run_analysis.R
@@ -0,0 +1,104 @@
+##  function setup.filedata() 
+##      Parameter:
+##          URL to download
+##
+##      Functionality:
+##          1. Decode the url and get the filename to download
+##          2. Download the file to /tmp folder in case that data folder or zip 
+##              file can not be found (it is not downloaded in the current
+##              work directory, but in /tmp folder)
+##          3. Unzip the data file in case that it be necessary
+##
+##      Return:
+##          The folder name where data resides
+##
+setup.filedata <- function(url) {
+    list <- strsplit( URLdecode(url), "/" )[[1]]
+    zipfile <- list[ length(list) ]
+    foldername <- sub(".zip$", "", zipfile)
+    
+    if ( file.exists( foldername ) ) return(foldername)
+    
+    if (! file.exists( zipfile )) {
+        tempfile <- paste0("/tmp/", zipfile)
+        
+        if (! file.exists( tempfile )) {
+            download.file( url, tempfile )
+            zipfile <- tempfile
+        }
+    }
+    unzip( zipfile )
+    foldername
+}
+
+folder <- setup.filedata("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip")
+
+#   1. Merges the training and the test sets to create one data set.
+
+set <- "train"
+filepaths <- paste(folder, set, paste0( c("X_", "y_", "subject_"), set, ".txt" ), sep = "/")
+trainVars <- read.table(filepaths[1])
+trainActivities <- read.table(filepaths[2])
+trainSubjects <- read.table(filepaths[3])
+
+set <- "test"
+filepaths <- paste(folder, set, paste0( c("X_", "y_", "subject_"), set, ".txt" ), sep = "/")
+testVars <- read.table(filepaths[1])
+testActivities <- read.table(filepaths[2])
+testSubjects <- read.table(filepaths[3])
+
+vars <- rbind(trainVars, testVars)
+activities <- rbind(trainActivities, testActivities)
+subjects <- rbind(trainSubjects, testSubjects)
+
+#   2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+
+features <- read.table(paste(foldername, "features.txt", sep = "/"))
+indices <- grep("-mean[^a-zA-Z]|-std[^a-zA-Z]", features[, 2])
+
+newdata <- vars[, indices]
+
+#   3. Uses descriptive activity names to name the activities in the data set
+
+activityLabels <- read.table(paste(foldername, "activity_labels.txt", sep = "/"))
+activities[, 1] <- activityLabels[ activities[, 1], 2 ]
+newdata <- cbind(subjects, activities, newdata)
+
+#   4. Appropriately labels the data set with descriptive variable names. 
+
+varnames <- gsub("[()]", "", c("Subject", "Activity", as.character(features[indices, 2])))
+names(newdata) <- varnames
+write.table(newdata, "training_and_test_set_merged.txt", row.names = FALSE)
+
+#   5. From the data set in step 4, creates a second, independent tidy data set 
+#       with the average of each variable for each activity and each subject.
+
+# Calculate the number of subjects, activities and variables (from the previous
+# data set, the one with variables related with mean and standard deviation)
+
+subjectCount <- nrow(unique(subjects))
+activityCount <- nrow(activityLabels)
+varCount <- length(indices)
+
+# Create the new data.frame with as many rows as combinations of subjects with
+# activities, and keep the names of the variables measured
+
+df <- data.frame( newdata[ 1:(subjectCount * activityCount), ] )
+
+# Initialize the combination, row 1 - subject id, row 2 - activity
+
+df[, 1] <- sort(rep(1:subjectCount, activityCount))
+df[, 2] <- activityLabels[ rep(1:activityCount, subjectCount), 2 ]
+
+# Impose an order in the activity 
+
+newdata$Activity <- factor(newdata$Activity, levels = activityLabels[, 2])
+
+# Iterate through all variables
+for (i in 1:varCount) {
+    # Compute the mean of all subject-activity combination 
+    colN <- aggregate(newdata[, i+2] ~ newdata[, 1] + newdata[, 2], newdata, mean)
+    colN <- colN[ order(colN[, 1], colN[, 2]), ]
+    df[, i+2] <- colN[, 3]
+}
+write.table(df, "average_of_mean_and_std.txt", row.names = FALSE)
